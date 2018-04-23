e0d5c5f90bb7bd9b65aae06f520eb5f9d7084ef3
zanas28 <asaduddin.zanky@gmail.com>
2018-04-24 06:48:37 +0700

    resolve navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,33 @@
-import React, {Component, Fragment} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
-class Navbar extends Component {
-    render() {
-        return(
-            <Fragment>
-                <AppBar title='Pixabay Image Finder'/>
-            </Fragment>
-        )
-    }
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function Navbar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Pixabay Image Finder
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default withStyles(Navbar);
+export default withStyles(styles)(Navbar);
